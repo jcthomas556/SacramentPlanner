e9956bac5a2b1a299faaedefeba726bfa6160be0
Jacob <jcthomas556@byui.com>
2019-07-09 11:32:32 -0600

    Initial Commit

---
+++ SacramentPlanner/SacramentPlanner/Models/Hymn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlanner.Models
+{
+   public class Hymn
+   {
+      [Range(0, 300)]
+      public int Page { get; set; }
+
+      [StringLength(50)]
+      public string Title { get; set; }
+
+      [StringLength(50)]
+      public string Type { get; set; }
+
+
+   }
+}
---
+++ SacramentPlanner/SacramentPlanner/Models/Planner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlanner.Models
+{
+   public class Planner
+   {
+
+      public int ID { get; set; }
+
+
+      [DataType(DataType.Date)]
+      [DisplayFormat(DataFormatString = "{0:MMMM dd, YYYY}", ApplyFormatInEditMode = true)]
+      [Display(Name = "Sacrament Date")]
+      public DateTime SacramentDate { get; set; }
+
+
+      
+      [StringLength(50)]
+      [Display(Name = "Ward")]
+      public string Ward { get; set; }
+
+
+   
+      [StringLength(50)]
+      [Display(Name = "Presiding")]
+      public string Presiding { get; set; }
+
+      
+      [StringLength(50)]
+      [Display(Name = "Conducting")]
+      public string Conducting { get; set; }
+
+  
+      [StringLength(50)]
+      [Display(Name = "Invocation")]
+      public string Invocation { get; set; }
+
+      public ICollection<Speaker> Speakers { get; set; }
+
+      public ICollection<Hymn> Hymns { get; set; }
+
+     
+
+     
+    
+
+   }
+}
